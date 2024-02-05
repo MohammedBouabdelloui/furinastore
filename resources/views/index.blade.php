c12da5b9a3a21592eb26db37fffea24018fb656a
AbdallahIsmaili <aismaili691@gmail.com>
2024-02-05 10:42:26 -0800

    update
    Starting the index store front-end, navbar and register modal done.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,11 +1,15 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1> hello </h1>
-</body>
-</html>
+@extends('layouts.web')
+ 
+@section('title', 'أفضل متجر عربي للعبة قنشن')
+ 
+@section('content')
+
+    <div class="mx-auto max-w-[1280px]">
+
+        <h1 class="text-3xl font-bold underline">
+            Hello world!
+        </h1>
+
+    </div>
+
+@endsection
