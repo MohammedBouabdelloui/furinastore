e95e97a4944e1e74299efceb50fdb5d1fc549057
MOHAMMED BOUABDELLAOUI <118568976+MohammedBouabdelloui@users.noreply.github.com>
2024-02-13 09:46:30 +0100

    update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,7 +22,7 @@
     <script src="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.js"></script>
     
     @vite('resources/css/app.css')
-    @notifyCss
+    {{-- @notifyCss --}}
 </head>
 <body class="bg-white">
 
