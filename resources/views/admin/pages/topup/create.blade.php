3b1d2b75462e06e12630c3eb162897cabea7adde
AbdallahIsmaili <aismaili691@gmail.com>
2024-02-14 09:59:20 -0800

    Update create.blade.php

--- resources/views/admin/pages/topup/create.blade.php
+++ resources/views/admin/pages/topup/create.blade.php
@@ -50,7 +50,7 @@
 
     <div class="mb-6">
       <label for="title" class="block mb-3 text-sm font-medium text-gray-500 ">عنوان المنتوج</label>
-      <input type="text" id="title" name="title" class="bg-white border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 outline-none  focus:border-transparent focus:ring-0" placeholder="8080 كرستالة" required />
+      <input type="text" id="title" name="title" class="bg-white border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 outline-none focus:ring-0" placeholder="8080 كرستالة" required />
     </div>
 
     <div class="mb-6">
@@ -60,7 +60,7 @@
        min="0" 
        id="price" 
        name="price" 
-       class="bg-white border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 outline-none focus:border-transparent focus:ring-0" 
+       class="bg-white border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 outline-none focus:ring-0" 
        placeholder="800 د.م" 
        required 
        pattern="[0-9]+(\.[0-9]+)?" 
@@ -70,56 +70,13 @@
 
     <div class="mb-6">
       <label for="topup_value" class="block mb-3 text-sm font-medium text-gray-500 ">القيمة المطلوبة</label>
-      <input type="number" min="0" id="topup_value" name="topup_value" class="bg-white border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 outline-none  focus:border-transparent focus:ring-0" placeholder="120" required />
+      <input type="number" min="0" id="topup_value" name="topup_value" class="bg-white border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 outline-none focus:ring-0" placeholder="120" required />
     </div>
 
     <label for="price" class="block mb-3 text-sm font-medium text-gray-500 ">شرح المنتوج</label>
 
     <div class="w-full mx-auto rounded-xl bg-white shadow-lg p-5 text-black mb-6" x-data="app()" x-init="init($refs.wysiwyg)">
-        <div class="border border-gray-200 overflow-hidden rounded-md">
-            <div class="w-full flex border-b border-gray-200 text-xl text-gray-600">
-                <button class="outline-none focus:outline-none border-r border-gray-200 w-10 h-10 hover:text-indigo-500 active:bg-gray-50" @click="format('bold')">
-                    <i class="mdi mdi-format-bold"></i>
-                </button>
-                <button class="outline-none focus:outline-none border-r border-gray-200 w-10 h-10 hover:text-indigo-500 active:bg-gray-50" @click="format('italic')">
-                    <i class="mdi mdi-format-italic"></i>
-                </button>
-                <button class="outline-none focus:outline-none border-r border-gray-200 w-10 h-10 mr-1 hover:text-indigo-500 active:bg-gray-50" @click="format('underline')">
-                    <i class="mdi mdi-format-underline"></i>
-                </button>
-                <button class="outline-none focus:outline-none border-l border-r border-gray-200 w-10 h-10 hover:text-indigo-500 active:bg-gray-50" @click="format('formatBlock','P')">
-                    <i class="mdi mdi-format-paragraph"></i>
-                </button>
-                <button class="outline-none focus:outline-none border-r border-gray-200 w-10 h-10 hover:text-indigo-500 active:bg-gray-50" @click="format('formatBlock','H1')">
-                    <i class="mdi mdi-format-header-1"></i>
-                </button>
-                <button class="outline-none focus:outline-none border-r border-gray-200 w-10 h-10 hover:text-indigo-500 active:bg-gray-50" @click="format('formatBlock','H2')">
-                    <i class="mdi mdi-format-header-2"></i>
-                </button>
-                <button class="outline-none focus:outline-none border-r border-gray-200 w-10 h-10 mr-1 hover:text-indigo-500 active:bg-gray-50" @click="format('formatBlock','H3')">
-                    <i class="mdi mdi-format-header-3"></i>
-                </button>
-                <button class="outline-none focus:outline-none border-l border-r border-gray-200 w-10 h-10 hover:text-indigo-500 active:bg-gray-50" @click="format('insertUnorderedList')">
-                    <i class="mdi mdi-format-list-bulleted"></i>
-                </button>
-                <button class="outline-none focus:outline-none border-r border-gray-200 w-10 h-10 mr-1 hover:text-indigo-500 active:bg-gray-50" @click="format('insertOrderedList')">
-                    <i class="mdi mdi-format-list-numbered"></i>
-                </button>
-                <button class="outline-none focus:outline-none border-l border-r border-gray-200 w-10 h-10 hover:text-indigo-500 active:bg-gray-50" @click="format('justifyLeft')">
-                    <i class="mdi mdi-format-align-left"></i>
-                </button>
-                <button class="outline-none focus:outline-none border-r border-gray-200 w-10 h-10 hover:text-indigo-500 active:bg-gray-50" @click="format('justifyCenter')">
-                    <i class="mdi mdi-format-align-center"></i>
-                </button>
-                <button class="outline-none focus:outline-none border-r border-gray-200 w-10 h-10 hover:text-indigo-500 active:bg-gray-50" @click="format('justifyRight')">
-                    <i class="mdi mdi-format-align-right"></i>
-                </button>
-            </div>
-            <div class="w-full">
-                <iframe x-ref="wysiwyg" class="w-full h-96 overflow-y-auto"></iframe>
-            </div>
-            <textarea id="description" name="description" style="display: none;"></textarea>
-        </div>
+        <textarea rows="15" id="description" name="description" class="w-full bg-white border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 outline-none focus:ring-0"></textarea>
     </div>
 
     <div class="mb-6">
@@ -137,33 +94,5 @@
 </form>
 
 
-<script>
-function app() {
-    return {
-        wysiwyg: null,
-        init: function(el) {
-            // Get el
-            this.wysiwyg = el;
-            // Add CSS
-            this.wysiwyg.contentDocument.querySelector('head').innerHTML += `<style>
-            *, ::after, ::before {box-sizing: border-box;}
-            :root {tab-size: 4;}
-            html {line-height: 1.15;text-size-adjust: 100%;}
-            body {margin: 0px; padding: 1rem 0.5rem;}
-            body {font-family: system-ui, -apple-system, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji";}
-            </style>`;
-            this.wysiwyg.contentDocument.body.innerHTML += `
-            <h1>شرح المنتوج</h1>
-            `;
-            document.getElementById('description').value = this.wysiwyg.contentDocument.body.innerHTML;
-            this.wysiwyg.contentDocument.designMode = "on";
-        },
-        format: function(cmd, param) {
-            this.wysiwyg.contentDocument.execCommand(cmd, !1, param||null)
-            document.getElementById('description').value = this.wysiwyg.contentDocument.body.innerHTML;
-        }
-    }
-}
-</script>
 
 @endsection
